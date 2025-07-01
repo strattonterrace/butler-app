8f0c055708635c20853146bc84df2a40a748841e
Nicholas Dallas <nicholasdallas@mac.wyomingseminary.org>
2025-06-30 22:09:28 -0400

    updated vite into project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-3xl font-bold">Hello, Personal Assistant App 👋</h1>
+    </div>
+  );
+}
+
+export default App;
